d3e391b5f637b44d1fbf70ab448331b46856e4f4
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-29 00:52:55 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,24 +31,24 @@ def can_be_created_in_a_block(args = __)
   # release_date == 1990
   
   Movie.create do |m|
-    __
+    
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.first 
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last 
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.size 
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.first(id) 
 end
 
 def can_find_by_multiple_attributes
